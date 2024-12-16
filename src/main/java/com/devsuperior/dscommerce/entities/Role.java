40c3631d44c27cbf32dfa6b256d2265937f778b2
Balieiro004 <Balieiro004@gmail.com>
2024-12-16 20:28:33 -0300

    UserDetailsService

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -2,10 +2,7 @@ package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
-
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "tb_role")
